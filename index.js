29dc3e67791a11070992df216a0f2a84d1feea85
Ryan F. Salerno <rfsalerno@gmail.com>
2018-05-15 11:09:51 -0400

    Add swiper styles, js, and markup

--- index.js
+++ index.js
@@ -37,3 +37,28 @@ $(document).ready(function() {
     $("#" + candidates[name]).html(val);
   }
 });
+
+setTimeout(function() {
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+}, 100);
+
